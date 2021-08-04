23d445264ca966c383b1f6fd7a1aae86d72bebff
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-04 06:23:21 -0700

    IInvertedIndex added

---
+++ Phase5/SearchEngine/SearchEngine/Interfaces/IInvertedIndex.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SearchEngine
+{
+    public interface IInvertedIndex
+    {
+        IInvertedIndex tokenizeFiles(Dictionary<string, string> allDocuments);
+        SortedSet<string> tokenize(string st);
+        SortedSet<string> query(UserInput input);
+        SortedSet<string> removeWordFromResult(string word, SortedSet<string> result);
+        SortedSet<string> addWordToResult(string word, SortedSet<string> result);
+        SortedSet<string> andWordWithResult(string word, SortedSet<string> result);
+        SortedSet<string> getDocsContainWord(string word);
+    }
+}
