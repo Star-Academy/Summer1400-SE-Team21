4ea64e2490a68736a9d602891881af85a47f9811
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-04 03:43:47 -0700

    File Reader added

---
+++ Phase5/SearchEngine/SearchEngine/FileReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SearchEngine
+{
+    public class FileReader : IFileReader
+    {
+        private Dictionary<string, string> _fileNameToItsStuffs = new Dictionary<string, string>();
+
+        public Dictionary<string, string> ReadingFiles(string folderName)
+        {
+            ListFilesForFolder(folderName);
+            return _fileNameToItsStuffs;
+        }
+
+        public void ListFilesForFolder(string path)
+        {
+            try
+            {
+                foreach (string file in Directory.EnumerateFileSystemEntries(path))
+                {
+                    FileAttributes attr = File.GetAttributes(file);
+
+                    if (attr.HasFlag(FileAttributes.Directory))
+                        ListFilesForFolder(file);
+                    else
+                    {
+                        string[] splited = file.Split('\\');
+                        string fileName = splited[splited.Length - 1];
+                        string[] lines = File.ReadAllLines(file);
+                        string stuffs = "";
+                        foreach (string line in lines)
+                            stuffs += line + " ";
+                        stuffs = stuffs.Trim();
+                        _fileNameToItsStuffs.Add(fileName, stuffs);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Path is not valid");
+            }
+        }
+        
+        
+    }
+}
---
+++ Phase5/SearchEngine/TestProject1/FileReaderTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using SearchEngine;
+using Xunit;
+
+namespace TestProject1
+{
+    public class FileReaderTest
+    {
+        [Fact]
+        public void ReadNotAvailableFolder() {
+            FileReader fileReader = new FileReader();
+            Dictionary<string, string> contents = fileReader.ReadingFiles("notAvailable");
+            Dictionary<string, string> expected = new Dictionary<string, string>();
+            Assert.Equal(contents, expected);
+        }
+
+        [Fact]
+        public void readSimpleFolder(){
+            FileReader fileReader = new FileReader();
+            Dictionary<string, string> content = fileReader.ReadingFiles("../../../TestProject1/TestDocs/simpleFolder");
+            Dictionary<string, string> expected = new Dictionary<string, string>();
+            expected.Add("simpleFile1.txt","this is simpleFile1");
+            expected.Add("simpleFile2.txt","this is simpleFile2");
+            Assert.Equal(expected,content);
+        }
+
+        [Fact]
+        public void readComplexFolder(){
+            FileReader fileReader = new FileReader();
+            Dictionary<string, string> content = fileReader.ReadingFiles("../../../TestProject1/TestDocs/complexFolder");
+            Dictionary<string, string> expected = new Dictionary<string, string>();
+            expected.Add("childFile1.txt","this is childFile1");
+            expected.Add("childFile2.txt","this is childFile2");
+            expected.Add("complexFile1.txt","this is complexFile1");
+            expected.Add("complexFile2.txt","this is complexFile2");
+            Assert.Equal(expected,content);
+        }
+
+
+        [Fact]
+        public void gettingFileAndReadIt (){
+            FileReader fileReader = new FileReader();
+            Dictionary<string, string> files = fileReader.ReadingFiles("../../../TestProject1/TestDocs/DocsForTest");
+
+            Assert.True(files.ContainsKey("firstFile"));
+            Assert.True(files["firstFile"].StartsWith("Hello Everyone"));
+        }
+    }
+}
