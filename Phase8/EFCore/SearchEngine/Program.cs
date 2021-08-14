8cdd30bdf7be9fd7ded3124ce17a61a456c50ddf
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-14 13:58:04 -0700

    some new tests added

--- Phase8/EFCore/SearchEngine/Program.cs
+++ Phase8/EFCore/SearchEngine/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace SearchEngine
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             new Manager().Run(args);
         } 
---
+++ Phase8/EFCore/TestProject1/ManagerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using SearchEngine;
+using Xunit;
+
+namespace TestProject1
+{
+    public class ManagerTest
+    {
+        [Fact]
+        public void JustTest()
+        {
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            StringReader reader = new StringReader("hahaha\nexit");
+            Console.SetIn(reader);
+
+            string[] args = new string[0];
+            new Manager().Run(args);
+            
+            Assert.Equal(writer.ToString(), "using In Memory\r\nstart engine\r\nno doc found\r\nend engine\r\n");
+        }
+        
+        
+        [Fact]
+        public void JustTest2()
+        {
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            StringReader reader = new StringReader("hahaha\nexit");
+            Console.SetIn(reader);
+
+            string[] args = new string[1];
+            args[0] = "../../../../TestProject1/TestDocs/Doc";
+            Program.Main(args);
+
+            Assert.Equal(writer.ToString().Substring(0, 5), "using");
+        }
+        
+    }
+}
