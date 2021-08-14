116ff1a140caa91d37c004d41d53af148908183a
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-14 06:14:04 -0700

    Phase6 ConsoleApp initialized

---
+++ Phase6/SearchEngineConsoleApp/SearchEngineConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchEngineConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
