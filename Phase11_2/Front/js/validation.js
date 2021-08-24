0d162fe27563503e3d155fa0767fb56a4e1208d0
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-24 10:32:26 -0700

    real time hint in front end completed

--- Phase11_2/Front/js/validation.js
+++ Phase11_2/Front/js/validation.js
@@ -9,11 +9,23 @@ function validate (){
         document.getElementById("searchError").style.fontSize = "14px";
         status = false;
     }
-    else if (document.getElementById("searchError").value != ""){
-        document.getElementById("searchError").innerHTML = "Phase Can Not Be Null";
-		document.getElementById("searchError").style.color = "blue";
-        document.getElementById("searchError").style.fontSize = "14px";
-        status = false;
+    else if (document.getElementById("search").value.length > 2){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+            document.getElementById("searchError").style.color = "blue";
+            document.getElementById("searchError").style.fontSize = "14px";
+            const myArr = (this.responseText).split("\"");
+            for (let i = 0; i<myArr.length; i++){
+                if (i % 2 == 1){
+                    document.getElementById("searchError").innerHTML += myArr[i] + "<br>";
+                }
+                if (i > 20){
+                    break;
+                }
+            }
+        }
+        xmlhttp.open("GET", "https://localhost:5001/Search/GetHints?hint=" + document.getElementById("search").value);
+        xmlhttp.send();
     }
 }
 
