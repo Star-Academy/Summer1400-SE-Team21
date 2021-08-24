f1eef020732e3575811b878cf69015ef708cab19
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-24 11:25:43 -0700

    Front files added to WebApplication

---
+++ Phase8/EFCore/WebApplication/wwwroot/Main/js/validation.js
@@ -0,0 +1,31 @@
+function validate (){
+    document.getElementById("searchError").innerHTML = "";
+
+    let status = true;
+
+    if (document.getElementById("search").value == ""){
+        document.getElementById("searchError").innerHTML = "Phase Can Not Be Null";
+		document.getElementById("searchError").style.color = "red";
+        document.getElementById("searchError").style.fontSize = "14px";
+        status = false;
+    }
+    else if (document.getElementById("search").value.length > 2){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.responseType = 'json';
+        xmlhttp.onload = function() {
+            document.getElementById("searchError").style.color = "blue";
+            document.getElementById("searchError").style.fontSize = "14px";
+            const res = this.response;
+            for (let i = 0; i<res.length; ++i){
+                document.getElementById("searchError").innerHTML += res[i] + "<br>";
+                if (i >= 10) break;
+            }
+        }
+        xmlhttp.open("GET", "https://localhost:5001/Search/GetHints?hint=" + document.getElementById("search").value);
+        xmlhttp.send();
+    }
+}
+
+function clean (){
+    document.getElementById("searchError").innerHTML = "";
+}
